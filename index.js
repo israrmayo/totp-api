f932ba4a63567509b1af5b9a38e5affa8dc3dc35
israrmayo <israrahmedmayo@gmail.com>
2025-07-12 08:19:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const speakeasy = require("speakeasy");
+
+app.get("/totp", (req, res) => {
+  const key = req.query.key;
+  if (!key) return res.status(400).json({ error: "Missing key" });
+
+  try {
+    const token = speakeasy.totp({
+      secret: key,
+      encoding: "base32"
+    });
+    res.json({ totp: token });
+  } catch (e) {
+    res.status(500).json({ error: "Invalid key" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("TOTP server running");
+});
